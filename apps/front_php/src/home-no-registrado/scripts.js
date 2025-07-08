c004ab122859301dddaf6dde02c49efbb64eb2b0
NachoRolu <ignaciorolu1305@gmail.com>
2025-07-07 21:48:53 -0300

    Home no registrado terminado (Falta Responsive)
    Termine la estructura del home no registrado. Agregue la seccion sobre nosotros y el footer. Falta hacerlo responsive y vincular los links y botones de iniciar sesion y registrarse a las paginas correspondientes.

--- apps/front_php/src/home-no-registrado/scripts.js
+++ apps/front_php/src/home-no-registrado/scripts.js
@@ -1,5 +1,5 @@
 window.addEventListener('scroll', function () {
-  const navbar = document.querySelector('.navbar');
+  const navbar = document.querySelector('nav');
   if (window.scrollY > 10) {
     navbar.classList.add('scrolled');
   } else {
