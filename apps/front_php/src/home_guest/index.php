b5090f4aafd9b3091c18bda4a2a4f4cb6039b1c2
NachoRolu <ignaciorolu1305@gmail.com>
2025-10-09 19:50:09 -0300

    Creacion archivos home visitante
    Cree la estructura basica del home visitante con todos sus archivos correpondientes. Cada seccion es un archivo independiente dentro de la carpeta componentes los cuales luego se incluyen dentro del index.php

---
+++ apps/front_php/src/home_guest/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Home Eskua</title>
+</head>
+<body>
+
+    <?php include 'components/navbar.php'; ?>
+    <?php include 'components/hero.php'; ?>
+    <?php include 'components/footer.php'; ?>
+
+</body>
+</html>
