f0b87ced8d6629c718e8b35545921c34c92e0268
NachoRolu <ignaciorolu1305@gmail.com>
2025-10-10 12:16:01 -0300

    Cree carpeta registrarse

---
+++ apps/front_php/src/register/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registrarse</title>
+    <link rel="stylesheet" href="../output.css">
+</head>
+<body>
+
+    <?php include 'components/register.html'; ?>
+
+</body>
+</html>
